417618a5daafeb4b383041716388e6f7c56cca38
omer.besentt <omer.besent@hotmail.com>
2021-03-22 02:02:34 +0300

    22.03.2021 saat 02.02

--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Entities.Concrete;
-
-namespace Business.Constans
+﻿namespace Business.Constans
 {
     public static class Messages
     {
@@ -14,5 +9,12 @@ namespace Business.Constans
         public static string ProductCountOfCategoryError = "Kategoriye eklenen ürün sayısı en fazla 15 olabilir";
         public static string ProductNameAllreadyExists = "Bu isimde zaten ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Geçersiz yetki";
+        public static string UserRegistered = "Kayıt oldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
     }
 }
